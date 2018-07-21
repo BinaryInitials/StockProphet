fa37926946b7e46bc746e66686634f90c1459c53
olivier.izad <olivier.i@us.ibm.com>
2018-07-21 11:25:26 -0400

    fixing bug in generating databse

--- js/searchDesktop.js
+++ js/searchDesktop.js
@@ -1,5 +1,5 @@
 function search() {
-var input, filter, table, tr, td1, td2, i;
+var input, filter, table, tr, td1, td2, td3, td4, i;
 input = document.getElementById("filter-search");
 filter = input.value.toUpperCase().split(" ");
 table = document.getElementById("myTable");
@@ -7,10 +7,12 @@ tr = table.getElementsByTagName("tr");
 for (i = 0; i < tr.length; i++) {
 td1 = tr[i].getElementsByTagName("td")[2];
 td2 = tr[i].getElementsByTagName("td")[3];
-if (td1 && td2) {
+td3 = tr[i].getElementsByTagName("td")[4];
+td4 = tr[i].getElementsByTagName("td")[5];
+if (td1 && td2 && td3 && td4) {
 var exist = false;
 for(j=0;j<filter.length;j++){
-if (td1.innerHTML.toUpperCase().indexOf(">"+filter[j]+"<") > -1 || td2.innerHTML.toUpperCase().indexOf(">"+filter[j]+"<") > -1 || filter[j] == "") {
+if (td1.innerHTML.toUpperCase().indexOf(">"+filter[j]+"<") > -1 || td2.innerHTML.toUpperCase().indexOf(">"+filter[j]+"<") > -1 || td3.innerHTML.toUpperCase().indexOf(">"+input.value.toUpperCase() +"<") > -1 || td4.innerHTML.toUpperCase().indexOf(">"+input.value.toUpperCase() +"<") > -1 || filter[j] == "") {
 exist = true;
 }
 }
@@ -21,4 +23,5 @@ tr[i].style.display = "none"
 }
 }
 }
+filterFunction();
 }
