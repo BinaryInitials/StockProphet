b8b83894b74c26829f86b30b7f82d317ee30f682
olivier.izad <olivier.i@us.ibm.com>
2018-09-25 23:28:59 -0400

    Adding more companies

--- src/com/stockprophet/main/GenerateCommonIndexes.java
+++ src/com/stockprophet/main/GenerateCommonIndexes.java
@@ -17,6 +17,12 @@ public class GenerateCommonIndexes {
 		indexMap.put("SPY", new String[]{"SPDR S&P 500 ETF Trust", "Index", "Index"});
 		indexMap.put("TQQQ", new String[]{"ProShares UltraPro QQQ", "Index", "Index"});
 		indexMap.put("SQ", new String[]{"Information Technology", "Internet Software & Services", "Square"});
+		indexMap.put("TEAM", new String[]{"Information Technology", "Internet Software & Services", "Atlasian"});
+		indexMap.put("MDB", new String[]{"Information Technology", "Internet Software & Services", "Mongo DB"});
+		indexMap.put("YEXT", new String[]{"Information Technology", "Internet Software & Services", "Yext"});
+		indexMap.put("SFIX", new String[]{"Information Technology", "Internet Software & Services", "Stitch Fix"});
+		indexMap.put("SNAP", new String[]{"Information Technology", "Internet Software & Services", "Snapchat"});
+		indexMap.put("YELP", new String[]{"Information Technology", "Internet Software & Services", "Yelp"});
 		indexMap.putAll(StockUtil.generateSPwithSectorsMap());
 		indexMap = StockUtil.cleanMap(indexMap);
 		return indexMap;
