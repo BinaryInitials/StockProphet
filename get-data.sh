106c39cb90e595acc1a3044084ed26e91b19c679
Olivier Izad <olivier@twp-inc.com>
2019-06-13 22:20:38 -0400

    Modifying get-data script

--- get-data.sh
+++ get-data.sh
@@ -14,15 +14,14 @@ then
 fi
 
 END_DATE=$(date +%s)
+START_DATE=$(echo /dev/null/ | awk '{print '$END_DATE'-5*365*24*60*60}')
 
-#1 year from today 
-START_DATE=$(echo /dev/null/ | awk '{print '$END_DATE'-370*24*60*60}')
+cookieJar="cookies.txt"
 
-cookieJar=$(mktemp)
-function getCrumb () {
-  echo -en "$(curl -s --cookie-jar $cookieJar $1)" | tr "}" "\n" | grep CrumbStore | cut -d':' -f 3 | sed 's/"//g'
-}
-curl -s --cookie $cookieJar  "https://query1.finance.yahoo.com/v7/finance/download/"$SYMBOL"?period1="$START_DATE"&period2="$END_DATE"&interval=1d&events=history&crumb="$(getCrumb "https://finance.yahoo.com/quote/$SYMBOL/?p=$SYMBOL") > data/$SYMBOL.csv
+crumb=$(curl -s --cookie-jar $cookieJar https://finance.yahoo.com/quote/$SYMBOL/?p=$SYMBOL | tr "}" "\n" | grep CrumbStore | cut -d':' -f 3 | sed 's/"//g')
+
+url="https://query1.finance.yahoo.com/v7/finance/download/"$SYMBOL"?period1="$START_DATE"&period2="$END_DATE"&interval=1d&events=history&crumb="$crumb
+curl -s --cookie $cookieJar $url > $SYMBOL.csv
 
 ((ATTEMPTS++))
-if grep "cookie" data/$SYMBOL.csv ; then echo $SYMBOL": Cookie Issues, new attempt..."; ./get-data.sh $1 $ATTEMPTS; else echo $SYMBOL": Success"; fi
+if grep "cookie" $SYMBOL.csv ; then echo $SYMBOL": Cookie Issues, new attempt..."; ./get-data.sh $1 $ATTEMPTS; else echo $SYMBOL": Success"; fi
